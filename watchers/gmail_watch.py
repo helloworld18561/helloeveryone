07e59703955d2dffcecb6b85db6c2e4802bd0bca
pruneplume <juicepooding@naver.com>
2026-06-13 21:38:38 +0900

    Add watchers/gmail_watch.py stub

---
+++ watchers/gmail_watch.py
@@ -0,0 +1,11 @@
+"""
+Gmail Watcher Stub
+
+This module will poll the user's Gmail account using the Gmail API and emit events
+for the event triage router. Currently this is a stub implementation.
+"""
+
+def handle_event(event):
+    """Handle a Gmail event (stub)."""
+    # TODO: implement event handling for new mail
+    return None
