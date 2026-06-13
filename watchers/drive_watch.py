e609d14a4daf61775e81d57b890b35d93cceb898
pruneplume <juicepooding@naver.com>
2026-06-13 21:38:56 +0900

    Add watchers/drive_watch.py stub

---
+++ watchers/drive_watch.py
@@ -0,0 +1,11 @@
+"""
+Google Drive Watcher Stub
+
+This module will monitor the user's Google Drive using the Drive API and emit events
+when new files or changes are detected. Currently this is a stub implementation.
+"""
+
+def handle_event(event):
+    """Handle a Google Drive event (stub)."""
+    # TODO: implement event handling for file changes
+    return None
